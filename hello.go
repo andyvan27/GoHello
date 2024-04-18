934470afd7c4d301267170ababbf744e86405dae
AndyN <121646012+andyvan27@users.noreply.github.com>
2024-04-18 21:46:23 +1000

    More from tour

--- hello.go
+++ hello.go
@@ -1,14 +1,57 @@
 package main
 
 import (
-	"fmt"
-
 	"example/user/hello/morestrings"
+	"fmt"
+	"math"
+	"math/cmplx"
+	"math/rand"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
+)
+
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
+var c, python, java bool = true, false, "no!" == "yes"
+
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
 	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+	fmt.Println("The time is", time.Now())
+	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Printf("Now you have %g problems.\n", math.Sqrt(7))
+	fmt.Println(math.Pi)
+	fmt.Println(add(42, 13))
+	fmt.Println(swap("Hello", "World"))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	fmt.Println(split(17))
+	var i int = 2
+	fmt.Println(i, c, python, java)
+	var j int = 1
+	k := 3
+	c, python, java := true, false, "no!"
+	fmt.Println(i, j, k, c, python, java)
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
